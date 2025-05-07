070fc8f63bf0db09f53ec260c6a450349ccfbd1d
Joao-Coelho5 <jp.cogui5@gmail.com>
2025-05-07 12:49:20 -0300

    tela login e detalhamentos em cadastro e index

--- Phantom-Aficionado-Site/main.js
+++ Phantom-Aficionado-Site/main.js
@@ -1,7 +1,7 @@
 function carregar(){
     var data = new Date()
     let ano = data.getFullYear()
-    let mes = data.getMonth()
+    let mes = data.getMonth() + 1
     var dia = data.getDate()
     let hora = data.getHours()
     div_dia.innerHTML = `${dia}/${mes}/${ano}`
@@ -9,3 +9,12 @@ function carregar(){
         corpo.style.backgroundImage = "url('Assets/P5city_night.jpg')"
     }
 }
+
+/* function phantom() {
+    let sel = sel_pt.value
+    if(sel == 'Joker'){
+        div_favorito.innerHTML = '<img src="https://media.tenor.com/U29670s_tS0AAAAM/persona5-joker.gif" alt="" srcset=""></img>'
+    } else {
+        div_favorito.innerHTML = ''
+    }
+} */
