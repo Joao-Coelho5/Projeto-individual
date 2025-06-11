3563dd8752966675f54b5de68f129ae1ad4cbf2f
Joao-Coelho5 <jp.cogui5@gmail.com>
2025-06-11 10:23:35 -0300

    Tirando itens não usados

--- BDPhantomAficionado/scriptBD-PA.sql
+++ BDPhantomAficionado/scriptBD-PA.sql
@@ -18,6 +18,15 @@ acertos			int not null,
 foreign key (usuarioId)references usuario(id)
 );
 
+CREATE TABLE noticias (
+    id 			int primary key auto_increment,
+    titulo 		varchar(200) not null,
+    descricao 	text not null,
+    link 		varchar(500) not null,
+    dataCadastro date
+);
+
+
 insert into usuario(nome, senha, email, dtNasc, personagemFavorito)
 			values('jotape8085', 'urubu100', 'swiplelubby@gmail.com', '2003-08-05', 'Akechi');
             
@@ -33,6 +42,8 @@ select u.personagemFavorito personagem, avg(q.acertos) acertos
 from quiz q
 join usuario u on u.id = q.usuarioId
 group by u.personagemFavorito;
+
+select titulo, descricao, dataCadastro, link from noticias;
 
 
 
